5ed7f70390d4656708bba0e56ef19bd50ceb95eb
gioantonucci <gioantonucci@hotmail.com>
2023-01-05 16:30:22 -0300

    total order

--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -5,6 +5,11 @@ import arrow from "@icons/flechita.svg";
 
 const MyOrder = () => {
   const { state } = useContext(AppContext);
+  const sumTotal = () => {
+    const reducer = (accumalator, currentValue) => accumalator + currentValue.price;
+    const sum = state.cart.reduce(reducer, 0);
+    return sum
+  };
   return (
     <aside className="my-order">
       <div className="title-container">
@@ -20,7 +25,7 @@ const MyOrder = () => {
           <p>
             <span>Total</span>
           </p>
-          <p>$150.00</p>
+          <p>${sumTotal()}</p>
         </div>
         <button class="primary-button">Checkout</button>
       </div>
